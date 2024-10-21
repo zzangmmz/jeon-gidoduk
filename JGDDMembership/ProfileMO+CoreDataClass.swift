371fda4a72c76c321f8a21c0e676da7a51c46717
mint3382 <mskim0926@naver.com>
2024-10-21 19:30:43 +0900

    feat: ProfileMO 구현

---
+++ JGDDMembership/ProfileMO+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileMO+CoreDataClass.swift
+//  JGDDMembership
+//
+//  Created by minsong kim on 10/21/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ProfileMO)
+public class ProfileMO: NSManagedObject {
+
+}
