4c0aa14cc6d789a297141b6ac8f6dec60c944aa4
Serena <sha0720@naver.com>
2024-10-21 16:59:08 +0900

    feat: CreateMemberViewController 생성

---
+++ JGDDMembership/JGDDMembership/View/CreateMemberViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CreateMemberViewController.swift
+//  JGDDMembership
+//
+//  Created by JGDDMembership on 10/21/24.
+//
+
+import UIKit
+
+final class CreateMemberViewController: UIViewController {
+    
+}
