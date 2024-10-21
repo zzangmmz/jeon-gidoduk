b71539d88dbb772588ea5f28a3057bdeaa56cd32
mint3382 <mskim0926@naver.com>
2024-10-21 16:31:07 +0900

    docs: 프로젝트 파일 생성

---
+++ JGDDMembership/JGDDMembership/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JGDDMembership
+//
+//  Created by minsong kim on 10/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
