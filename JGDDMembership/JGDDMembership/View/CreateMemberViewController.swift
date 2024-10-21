f5bc9d796fb48d6a932c41a09b403b54f53538fa
zzangmmz <leemj036@gmail.com>
2024-10-21 21:50:22 +0900

    feat: 버튼 클릭 시 버튼 색상 변경

--- JGDDMembership/JGDDMembership/View/CreateMemberViewController.swift
+++ JGDDMembership/JGDDMembership/View/CreateMemberViewController.swift
@@ -9,4 +9,32 @@ import UIKit
 
 final class CreateMemberViewController: UIViewController {
     
+    private let createMemberView = CreateMemberView()
+    
+    override func loadView() {
+        view = createMemberView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupAddTarget()
+    }
+    
+    private func setupAddTarget() {
+        [createMemberView.eButton, createMemberView.iButton, createMemberView.sButton, createMemberView.nButton, createMemberView.tButton, createMemberView.fButton, createMemberView.jButton, createMemberView.pButton].forEach { $0.addTarget(self, action: #selector(mbtiButtonTapped), for: .touchUpInside) }
+    }
+    
+    
+    // 이중선택 막아야 함
+    @objc func mbtiButtonTapped(_ button: UIButton) {
+        if button.backgroundColor == .gray {
+            button.backgroundColor = .clear
+        } else if button.backgroundColor == .clear {
+            button.backgroundColor = .gray
+        }
+    }
 }
+
+
+
