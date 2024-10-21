8b234fe3a09b90fedb11197208badcb2ec0515db
sikwan <yysskk99999@naver.com>
2024-10-21 17:51:09 +0900

    tableview ing

--- JGDDMembership/JGDDMembership/View/ViewController.swift
+++ JGDDMembership/JGDDMembership/View/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+   
+    
+    var tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+            
+        return tableView
+    }()
+    
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView = UITableView(frame: CGRect(x:0,y:0,width:self.view.bounds.width,height:self.view.bounds.height))
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.view.addSubview(tableView)
+        
+        
+        
+        
+        
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return ""
+    }
+    
+    
 }
+
+
 
